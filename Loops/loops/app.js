157752b8cf14cefde4e7ae8cc5864a6cc2d59f27
KimberlyCameron79 <110206975+KimberlyCameron79@users.noreply.github.com>
2022-08-11 16:05:29 -0400

    loopsnparameters

---
+++ Loops/loops/app.js
@@ -0,0 +1,76 @@
+/*
+
+//inside parentheis we have
+//three statements:
+
+//statement 1:
+//declare and initialized a variable
+//scoped to the for loop
+
+//statement 2:
+//a condition that when true runs the 
+//code in the for loop
+
+//statement 3:
+//incrementing of the variable in
+//order to eventually break the loop
+
+for (let i = 0; ; i++) {
+    console.log(i)
+    if(i > 10){
+        break;
+    }
+}
+
+// 1: declare and initialize variable
+// 2: check if condition is true
+// 3: console log runs and prints 0
+// 4: variable i increments and becomes 0 to 1
+// 5: check if condition is true or repeat step 2
+// 6: repeat step 3
+// 7: repeat step 4
+// 8: check if condition is true (it's not)
+// 9: exit loop, continue with rest of code
+
+//  break
+//  continue
+*/
+/*
+for (let j = m; j >- 0; j -- ) {
+    console.log (j)
+}
+*/
+/*
+for (let k = 1; k < 10; k = k + 2)
+{
+    console.log(k)
+}
+    for (let l=2; l<11; l=l+2)
+    {
+        console.log(l)
+    }
+
+*/
+/*
+for(let m=6; m<61;m=m+3)
+    {
+        console.log(m)
+    }
+*/
+/*
+for (let j= 0) 
+{
+console.log ('2')
+console.log ('23')
+console.log ('234')
+console.log ('2345')
+console.log('23456'))
+console.log('234567')
+console.log('23456789'))
+}
+*/
+let i=0;
+while(i<35 && i % 3 == 0){
+console.log (i+=3)
+
+}
