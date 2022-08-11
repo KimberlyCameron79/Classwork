d93532c59c5bdf4c4ba924517a53b5b3b27f3ce4
KimberlyCameron79 <110206975+KimberlyCameron79@users.noreply.github.com>
2022-08-11 16:09:47 -0400

    Classwork

---
+++ functions/app.js
@@ -0,0 +1,50 @@
+/*
+//function declaration
+function sayHello(){
+
+}
+
+//function expression
+const sayHi= function(){
+    return 'Hi!'
+}
+
+const sayHey =() => 'Hey'
+
+console.log (sayHello)
+console.log (sayHi)
+console.log (sayHey)
+*/
+/*
+function add(num1, num2) {  //() list of parameters
+    //num1   2
+    //num2   3
+    return num1 + num2
+}
+
+//add()   5
+
+console.log(add(2,3))  // ()list of arguments
+console.log(add(5,5)
+console.log(add(10,1))
+console.log(add(3,8))
+*/
+/*
+//compute area
+function area =(len,width){
+    return "The area of rectangle of width "+width+" and length of "+ len +" is :" + len *width+"sq unit"
+}
+console.log(area(8,15))
+*/
+/*
+function planetHasWater(planet) {
+    if (planet == 'Earth' || planet == 'Mars'){
+        return 'true';
+    } else {
+    return 'false';
+   }
+}
+
+console.log(planetHasWater('Mars'))
+console.log(planetHasWater('Jupiter'))
+*/
