bc7049226c2e5b11c4ae377850efdaf01d042708
KimberlyCameron79 <110206975+KimberlyCameron79@users.noreply.github.com>
2022-08-18 14:56:40 -0400

    review

---
+++ Review/review.js
@@ -0,0 +1,23 @@
+/* SECTION Q + A
+let Dinner = []
+
+function cookDinner () {
+
+
+
+const myCheese;
+myCheese= 3;
+const myBread;
+myBread=myCheese;
+
+declare();  assign(); define()
+
+const myGrilledTrout = Protein;
+
+}
+*/
+
+const firstVariable(); {
+
+    let firstVariable= 'Hello World!'
+}
---
+++ Review/script.js
@@ -0,0 +1,24 @@
+/*
+Section Q + A
+1. How do we assign a value to a variable? With the assignment operator. 
+
+2. How do we change the value of a variable? Setting the parameter value in the new function. 
+function start(){ variable=40
+}
+
+3. How do we assign an existing variable to a new variable? Using the assignment operator you can assign the value from an existing variable to another variable.
+const myCheese;
+myCheese= 3;
+const myBread;
+myBread=myCheese;
+
+4. Remind me, what are declare, assign, and define? Declare is creating the variable.  Assign function copies all enumerable own properties from one or more source objects to a target object. A define is a function used to load the modules which are objects, functions or class.
+function();  assign(); define();
+
+5.What is pseudocoding and why should you do it?  Pseudo coding is referred to as fake code but utilized to write programs in an informal way by making the rules yourself.
+
+SECTION STRINGS
+
+1. Create a variable called firstVariable. const firstVariable;
+
+2.Assign the 
