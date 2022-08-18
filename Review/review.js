02a8c765bc4ffb63e8e7df5efe67918e7341396e
KimberlyCameron79 <110206975+KimberlyCameron79@users.noreply.github.com>
2022-08-18 15:42:49 -0400

    review

--- Review/review.js
+++ Review/review.js
@@ -31,5 +31,46 @@ secondVariable = 'Shopping'
 
  console.log(`Hello, my name is ${yourName}`)
 
-   
+//SECTION C BOOLEANS
+
+const a = 4;
+const b = 53;
+const c =57;
+const d = 16;
+const e = 'Kevin'
+
+console.log(a !== b);
+console.log(c !== d);
+console.log('Name' === 'Name');
+
+console.log(true !== false);
+console.log(false || false || false || false || false || true);
+console.log(false === false)
+console.log(e === 'Kevin');
+console.log(a + b === c);
+console.log(a * a === d);
+console.log(48 !== '48');
+
+// Section D farm
+
+let animal = 'cow'
+
+if (animal === 'cow') {
+   console.log('moooo');
+} else {
+    console.log('Hey! You\'re not a cow')
+}
+
+//DRIVER'S ED
+
+let age = 16
+
+if (age >= 16){
+
+    console.log('Here are the keys!')
+} else{
+    console.log('Sorry, you\'re too young')
+}
+
+//LOOPS
 
