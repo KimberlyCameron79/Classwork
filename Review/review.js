1c04ec8a46546c9b89b39f8d66383e55e9b2aa74
KimberlyCameron79 <110206975+KimberlyCameron79@users.noreply.github.com>
2022-08-22 18:33:09 -0400

    review

--- Review/review.js
+++ Review/review.js
@@ -369,7 +369,20 @@ let maxOfThree = function(numOne, numTwo, numThree) {
 
 user.location ="Garfield";
 
+// D. Shopaholic
+//Our user has purchased an item!
+//They have purchased some "carbohydrates". Using .push(), add the string "carbohydrates" to the purchased array.
+//Our user has purchased an item!
+//They have purchased some "peace of mind". Using .push(), add the string "peace of mind" to the purchased array.
+//Our user has purchased an item!
+//They have purchased some "Merino jodhpurs". Using .push(), add the string "Merino jodhpurs" to the purchased array.
+//Console.log just the "Merino jodhpurs" from the purchased array.
 
+user.purchased.push ("carbohydrates");
+user.purchased.push ("peace of mind");
+user.purchased.push ("Merino jodhpurs");
+
+console.log(user.purchased[2]);
 
 
 
