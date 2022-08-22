b9dcea8db3d4745c86b8a9e7a62be343e86aa7bc
KimberlyCameron79 <110206975+KimberlyCameron79@users.noreply.github.com>
2022-08-22 18:42:00 -0400

    review

--- Review/review.js
+++ Review/review.js
@@ -384,6 +384,21 @@ user.purchased.push ("Merino jodhpurs");
 
 console.log(user.purchased[2]);
 
+//E. Object-within-Object
 
+
+user.friend = {
+    name: "Danielle",
+    age: 0,
+    location:"Lawrenceville",
+    purchased: []
+}
+
+console.log(user.friend.name);
+console.log(user.friend.location);
+user.friend.age = 55;
+user.friend.purchased.push("The One Ring");
+user.friend.purchased.push("A latte");
+console.log(user.friend.purchased[1]);
 
 
